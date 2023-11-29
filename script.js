55e55f6dff3162cda43e615922b1eb48f1e60c58
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2023-11-29 12:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,31 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () => reject(new Error(`Failed to load image's URL: ${image.url}`));
+    img.src = image.url;
+  });
+}
+
+function displayImages(images) {
+  output.innerHTML = '';
+  images.forEach((image) => {
+    output.appendChild(image);
+  });
+}
+
+btn.addEventListener("click", () => {
+  const promises = images.map(downloadImage);
+
+  Promise.all(promises)
+    .then((downloadedImages) => {
+      displayImages(downloadedImages);
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+});
